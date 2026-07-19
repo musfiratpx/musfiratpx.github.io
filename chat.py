600135150cf47c250062306b4b38eede0edffc18
musfirat <musfirat7777@gmail.com>
2026-07-18 21:06:44 -0400

    comments

--- chat.py
+++ chat.py
@@ -20,6 +20,11 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client() 
 #tavily
 tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-#nasa
-NASA_API_KEY = os.getenv("NASA_API_KEY")
 
+#Set up tavily so that it can receive client request on front end (click on planet)
+#tavily develops a query to send to gemini
+#Ex. User clicks Jupiter --> JavaScript sends a message to flask "Hey user clicked on planet 5."
+#Flask tells python (Tavily) --> Create a query with user input 5 -> Jupiter.
+#Tavily creates a query "What are some mathematically significant facts about Jupiter", and provides sources to gemini. 
+#gemini listens to instructions and sends an output that will display to front end as a pop-up. 
+#Gemini's instructions: Mathematically significant facts about user input relating to astronomy, space, or time physics. Condensed in a paragraph. Cites the sources. 
